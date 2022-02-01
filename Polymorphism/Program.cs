a3f09525f901b9b2646fd5b004dfd99902b4f9f9
thirupathi <thirupathi.narayanan@disys.com>
2022-02-01 17:27:54 +0530

    Add project files.

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,53 @@
+﻿/*uag System;
+namespace Studytonight
+{
+    public class Interest
+    {
+        public double MyBank(double amount, double rate)
+        {
+            return amount + (amount * rate);
+        }
+        public double MyBank(double amount, double rate, string holdertype)
+        {
+            return amount + (amount * rate) + 5000;
+        }
+    }
+    public class Bank_Interest
+    {
+        public static void Main(string[] args)
+        {
+            Interest obj = new Interest();
+
+            double final = obj.MyBank(50000, 0.1);
+            Console.WriteLine("Normal interest " + final);
+
+            final = obj.MyBank(50000, 0.5, "Express");
+            Console.WriteLine("Express interest  " + final);
+
+        }
+    }
+}*/
+using System;
+	class interest
+    {
+		public double total;
+		public interest(int cost, double rate)
+        {
+			total = cost + (rate * rate);
+			
+        }
+		public static interest operator -(interest b)
+        {
+			b.total = -b.total;
+        Console.WriteLine("Interest amount " + b.total);
+    }
+    }
+	class Class1
+	{
+		public static void Main()
+        {
+			interest i = new interest(50000, 0.5);
+			i = -i;
+			
+        }
+	}
